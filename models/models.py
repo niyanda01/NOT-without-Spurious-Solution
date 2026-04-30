389145f44224461e42a10437e5a7335ac961f13b
Anonymous <anonymous@example.com>
2026-04-30 15:14:12 +0900

    models update

--- models/models.py
+++ models/models.py
@@ -4,22 +4,28 @@ class Transport(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(2, 128),
+            nn.Linear(3, 64),
             nn.ReLU(),
-            nn.Linear(128, 2)
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2)
         )
 
     def forward(self, x):
-        return self.net(x)
+        z = torch.randn(x.shape[0], 1, device=x.device)
+        # z = 2 * torch.rand(x.shape[0], 1, device=x.device) - 1
+        return self.net(torch.cat([x, z], dim=1))
 
 
 class Critic(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(2, 128),
+            nn.Linear(2, 64),
             nn.ReLU(),
-            nn.Linear(128, 1)
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
         )
 
     def forward(self, x):
