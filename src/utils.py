a4dcbea8d3e9864fe9d56cadb7696b62b215dc38
Anonymous <anonymous@example.com>
2026-05-03 20:33:00 +0900

    update

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
+import math
+import matplotlib.pyplot as plt
+import torch 
+
 def cost(x, y):
     return ((x - y) ** 2).sum(dim=1, keepdim=True)/2
 
-def show_mapping(T,sample_mu,sample_nu):
+def show_mapping(T,sample_mu,sample_nu, device = "cpu"):
     x = sample_mu(1000).to(device)
 
     outputs = []
@@ -19,3 +23,12 @@ def show_mapping(T,sample_mu,sample_nu):
     plt.axis('equal')
     plt.legend()
     plt.show()
+
+def grad_norm(grads):
+    return torch.sqrt(sum((g**2).sum() for g in grads))
+
+def cosine_lr(step, total_steps, lr_max, lr_min=0.0):
+    return lr_min + 0.5 * (lr_max - lr_min) * (
+        1 + math.cos(math.pi * step / (step+total_steps))
+    )
+
